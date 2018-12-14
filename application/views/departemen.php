d9bbb069aaee47ccc085c970373bffd1c61f2481
Ulhakun <ullahkun@gmail.com>
2018-12-14 20:54:34 +0700

    first Commit

---
+++ application/views/departemen.php
@@ -0,0 +1,96 @@
+<!-- Content Wrapper. Contains page content -->
+
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Departemen
+        <small></small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Admin Panel</a></li>
+        <li class="active">Departemen</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+    <div class="row">
+        <div class="col-md-6">
+    <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title">Daftar Departemen</h3>
+            </div>
+            
+           
+            <div class="input-group margin">
+                
+                <!-- /btn-group -->
+                
+                <input type="text" class="form-control" placeholder="Masukkan nama departemen...">
+                <div class="input-group-btn">
+                  <button type="button" class="btn btn-success">Tambah</button>
+                </div>
+              </div>
+          
+            <div class="box-body">
+              <table class="table table-bordered">
+                <tr>
+                  <th style="width: 10px">No.</th>
+                  <th>Fakultas</th>
+                  <th style="width: 160px">Aksi</th>
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Matematika dan Ilmu Pengetahuan</td>
+                  <td>
+                    <a href="" data-toggle="modal" data-target="#modal-edit" class="btn btn-warning btn-sm fa fa-edit"></button></a> <a href="" class="btn btn-danger btn-sm fa fa-trash"></button></a>
+                  </td>
+                  
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Update software</td>
+                  <td>
+                    <a href="" data-toggle="modal" data-target="#modal-edit" class="btn btn-warning btn-sm fa fa-edit"></button></a> <a href="" class="btn btn-danger btn-sm fa fa-trash"></button></a>
+                  </td>
+                  
+                </tr>
+                <tr>
+                  <td>1.</td>
+                  <td>Update software</td>
+                  <td>
+                    <a href="" data-toggle="modal" data-target="#modal-edit" class="btn btn-warning btn-sm fa fa-edit"></button></a> <a href="" class="btn btn-danger btn-sm fa fa-trash"></button></a>
+                  </td>
+                  
+                </tr>
+              </table>
+            </div>
+
+            <div class="modal fade" id="modal-edit">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Edit Data</h4>
+              </div>
+              <div class="modal-body">
+                <p>One fine body&hellip;</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+            <!-- /.box-body -->
+            
+          </div></div></div>
+    </section>
+    <!-- /.content -->
+
+  <!-- /.content-wrapper -->
